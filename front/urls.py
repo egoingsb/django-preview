74f5ad7057be7c08a931077b9046e976c70e93d9
egoing <egoing@gmail.com>
2021-11-25 10:52:20 +0900

    경로에 슬래쉬 추가

--- front/urls.py
+++ front/urls.py
@@ -3,9 +3,9 @@ import front.views
 
 urlpatterns = [
     path('', front.views.index),
-    path('read/<int:id>', front.views.read), 
+    path('read/<int:id>/', front.views.read), 
     path('create/', front.views.create), 
-    path('update/<int:id>', front.views.update), 
-    path('delete/<int:id>', front.views.delete), 
+    path('update/<int:id>/', front.views.update), 
+    path('delete/<int:id>/', front.views.delete), 
 ]
 
